22b0de4fe616664367ce2b7ff4ec0fbc0fac5c0b
OleksiySolomakha <alekseysomik@gmail.com>
2026-02-15 19:59:48 +0200

    added some  changes for config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  defaultCommandTimeout: 10000,
+  viewportWidth: 1280,
+  viewportHeight: 720,
 
   e2e: {
+    // baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
